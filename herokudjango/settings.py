8779c51c65222979e29029655907b9aee073c86b
kiba <kibablog.net@gmail.com>
2021-06-11 12:00:31 +0900

    databases

--- herokudjango/settings.py
+++ herokudjango/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 # import django-heroku
-import dj_dataase_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import morning.apps
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'herokudjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
+        'PORT': '',
     }
 }
 
@@ -137,3 +148,6 @@ except ImportError:
 
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
+
+db_from_venv = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_venv)
